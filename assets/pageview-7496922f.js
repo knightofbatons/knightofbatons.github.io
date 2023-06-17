b5d651534761efa4bf15fed4950255975727e185
knightofbatons <knightofbatons@hotmail.com>
2023-06-17 11:25:34 +0000

    Deploying to gh-pages from @ knightofbatons/knightofbatons.github.io@67f7e49f539650c81fc69ed4135f7874717b48be 🚀

--- assets/pageview-7496922f.js
+++
@@ -1,1 +0,0 @@
-import{a8 as e}from"./app-87f51d11.js";const a=()=>e({serverURL:"https://waline.nerv.love"});export{a as updatePageview};
---
+++ assets/pageview-cdeee0a9.js
@@ -0,0 +1,1 @@
+import{a8 as e}from"./app-9206042d.js";const a=()=>e({serverURL:"https://waline.nerv.love"});export{a as updatePageview};
